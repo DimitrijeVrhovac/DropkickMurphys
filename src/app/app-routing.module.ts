41a0d1fd3feaf3f4d319c1582e6ae7b0baba7b25
Dimitrije Vrhovac <dima.vrhovac@gmail.com>
2023-07-12 14:54:17 +0200

    Contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GalleryComponent } from './gallery/gallery.component';
