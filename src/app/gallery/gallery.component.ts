feb8ad825fca9737b8d471aff29487a99804ca39
Dimitrije Vrhovac <dima.vrhovac@gmail.com>
2023-07-13 20:17:40 +0200

    gallery Slider

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Image, ReadJsonService } from '../read-json.service';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
 
+  viewSlider = false;
+  images: Image[] = [];
+  index = 0
+
+  constructor(private service: ReadJsonService) {}
+
+  showImage() {
+    this.service.getImage()
+      .subscribe((data:Image[]) => this.images = data);
+  }
+  ngOnInit(): void {
+    this.showImage();
+  }
+  showSlider(i:number) {
+    this.viewSlider = true
+    this.index = i
+  }
+  closeSlider() {
+    this.viewSlider = false
+  }
+  leftImage() {
+    this.index --
+    if(this.index <= 0 ) {
+      this.index = this.images.length
+    }
+  }
+  rightImage() {
+    this.index ++
+    if(this.index >= this.images.length) {
+      this.index = 0
+    } 
+  }
 }
--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,22 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Image, ReadJsonService } from '../read-json.service';
-
+import { Component, Input } from '@angular/core';
+import { Image } from '../read-json.service';
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.css']
 })
-export class ImageComponent implements OnInit {
-  
-  images: Image[] = [];
-
-  constructor(private service: ReadJsonService) {}
-
-  showImage() {
-    this.service.getImage()
-      .subscribe((data:Image[]) => this.images = data);
-  }
-  ngOnInit(): void {
-    this.showImage();
-  }
+export class ImageComponent  {
+    @Input() image:Image = {image:'',title:''};
 }
